19e5405c4ab99257e370a976b05274b968022316
Iskander <logick7@mail.ru>
2023-09-03 23:08:48 +0300

    23

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = "QRKot"
     description: str = "Благотворительный фонд поддержки котиков"
-    # pytest пишет что только по умолчанию может пройти название:'sqlite+aiosqlite'
     database_url: str = "sqlite+aiosqlite"
 
     secret: str = "SECRET"
